7b182a6a12ba326b136a6b2d765c90d3d67e6de8
vishwajeet-8 <109028770+vishwajeet-8@users.noreply.github.com>
2024-02-29 00:36:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+const PORT = process.env.PORT || 8000;
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`server running at http://localhost:${PORT}`);
+});
